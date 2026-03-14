21b4884fca31868eec12dab3d1f44d947765471c
Sandeep <sandeepnegi686@gmail.com>
2026-03-14 22:12:19 +0530

    added 2 files

---
+++ server/src/Types/MovieType.ts
@@ -0,0 +1,11 @@
+import { JwtPayload } from "jsonwebtoken";
+
+interface AuthPayload extends JwtPayload {
+  user?: {
+    _id: string;
+    name: string;
+    email: string;
+  };
+}
+
+export default AuthPayload;
