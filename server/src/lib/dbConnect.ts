b94677e5e5395b0687fae07f314bbc869d005d4f
Sandeep <sandeepnegi686@gmail.com>
2026-03-15 11:58:35 +0530

    updated dbConnection

--- server/src/lib/dbConnect.ts
+++ server/src/lib/dbConnect.ts
@@ -1,26 +1,46 @@
-import mongoose from "mongoose";
+import mongoose, { Connection } from "mongoose";
 
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+interface MongooseCache {
+  conn: Connection | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
-export default async function dbConnect(DB_URL: string) {
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB(db_url: string) {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(DB_URL, {
-      dbName: "netflix-clone",
+    const opts = {
       bufferCommands: false,
+      // For serverless, smaller pool sizes are often recommended
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose.connect(db_url, opts).then((mongoose) => {
+      return mongoose;
     });
   }
 
-  cached.conn = await cached.promise;
+  try {
+    const mongooseInstance = await cached.promise;
+    cached.conn = mongooseInstance.connection;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
   return cached.conn;
 }
+
+export default connectDB;
