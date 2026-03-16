5a19ddd81d5a164825bf53f9dcbf20ac87c368b7
Sandeep <sandeepnegi686@gmail.com>
2026-03-16 11:47:38 +0530

    fixed build issue.

---
+++ client/app/_components/AuthHandler.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function AuthHandler() {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    const token = params.get("access-token");
+
+    if (!token) return;
+
+    fetch(`/api/user/currentUser`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({ token }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      router.push("/profiles");
+    });
+  }, [params, router]);
+  return <div></div>;
+}
